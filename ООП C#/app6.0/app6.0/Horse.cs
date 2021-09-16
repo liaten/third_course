040b8216a76a78fdc4658c90d1c26d4828990a1d
Vladislav <vlad171vlad@gmail.com>
2021-09-16 17:06:19 +0300

    Add files via upload

---
+++ ООП C#/app6.0/app6.0/Horse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace app6._0
+{
+    class Horse
+    {
+        private int its_age;
+        public void Gallop()
+        {
+            Console.WriteLine("Gallopping...");
+        }
+        public virtual void Fly()
+        {
+            Console.WriteLine("Horses can't fly.");
+        }
+    }
+}

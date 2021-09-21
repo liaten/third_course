09000bf5cd8f88aa443056c275ec14ed885c3016
liaten <vlad171vlad@gmail.com>
2021-09-21 23:19:16 +0300

    InSec cases 2-5

---
+++ Object Oriented Programming in C #/app6.0/app6.0/Pegasus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace app6._0
+{
+    class Pegasus : Horse
+    {
+        public virtual void Fly()
+        {
+            Console.WriteLine("I can fly!!");
+        }
+    }
+}
---
+++ Object Oriented Programming in C #/app6.0/app6.0/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace app6._0
+{
+    class Program
+    {
+        const int Number_Of_Horses = 5;
+        static void Main(string[] args)
+        {
+            Horse[] Ranch = new Horse[Number_Of_Horses];
+            Horse P_Horse = new Horse();
+            int choice, i;
+            for (i = 0; i < Number_Of_Horses; i++)
+            {
+                Console.WriteLine("(1) - horse (2) - pegas");
+                choice = Convert.ToInt32(Console.ReadLine());
+                if(choice == 2)
+                {
+                    P_Horse = new Pegasus();
+                }
+                else
+                {
+                    P_Horse = new Horse();
+                }
+                Ranch[i] = P_Horse;
+            }
+            for (i = 0; i < Number_Of_Horses; i++)
+            {
+                Ranch[i].Fly();
+                // delete ranch[i];
+            }
+        }
+    }
+}

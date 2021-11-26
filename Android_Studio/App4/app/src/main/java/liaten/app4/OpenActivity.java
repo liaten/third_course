969bd497d811c02c3904da96a6d3f38e667009dd
liaten <vlad171vlad@gmail.com>
2021-11-26 05:36:50 +0300

    android4lab

--- Android_Studio/App4/app/src/main/java/liaten/app4/OpenActivity.java
+++ Android_Studio/App4/app/src/main/java/liaten/app4/OpenActivity.java
@@ -1,6 +1,7 @@
 package liaten.app4;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,12 +15,17 @@ public class OpenActivity extends Activity {
         Button csv_button = (Button) findViewById(R.id.csv);
         Button xml_button = (Button) findViewById(R.id.xml);
         Button json_button = (Button) findViewById(R.id.json);
+        txt_button.setOnClickListener(txt_listener);
+        csv_button.setOnClickListener(csv_listener);
+        xml_button.setOnClickListener(xml_listener);
+        json_button.setOnClickListener(json_listener);
 
     }
     private View.OnClickListener txt_listener = new View.OnClickListener(){
         @Override
         public void onClick(View view) {
-
+            Intent addIntent = new Intent("android.intent.action.TxtEditActivity");
+            startActivity(addIntent);
         }
     };
     private View.OnClickListener csv_listener = new View.OnClickListener(){
---
+++ Android_Studio/App4/app/src/main/java/liaten/app4/TxtEditActivity.java
@@ -0,0 +1,58 @@
+package liaten.app4;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class TxtEditActivity extends Activity  {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.txt_edit_activity);
+
+        // выгрузить текст из файла
+        EditText et = (EditText) findViewById(R.id.editTextTextMultiLine);
+        FileInputStream f = null;
+        try{
+            f = openFileInput("1.txt");
+            byte[] bytes = new byte[f.available()];
+            f.read(bytes);
+            String text = new String(bytes);
+            et.setText(text);
+        }
+        catch (IOException ex)
+        {
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+
+        // добавить текст в файл
+        Button push_txt = (Button) findViewById(R.id.push_button);
+        push_txt.setOnClickListener(txt_export_listener);
+    }
+    private View.OnClickListener txt_export_listener = new View.OnClickListener(){
+        @Override
+        public void onClick(View view) {
+            FileOutputStream f = null;
+            try
+            {
+                EditText et = (EditText) findViewById(R.id.editTextTextMultiLine);
+                String text = et.getText().toString();
+                f = openFileOutput("1.txt", MODE_PRIVATE);
+                f.write(text.getBytes());
+                Toast.makeText(TxtEditActivity.this,"Файл сохранен", Toast.LENGTH_SHORT).show();
+            }
+            catch (IOException ex)
+            {
+                Toast.makeText(TxtEditActivity.this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+}

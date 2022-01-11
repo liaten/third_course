315f21beaed3e8e193aec53cd46b6afc47dd3c60
liaten <vlad171vlad@gmail.com>
2022-01-11 23:35:05 +0300

    huge integer

---
+++ Object Oriented Programming in C #/app7.5.1/app7.5.1/HugeInteger.cs
@@ -0,0 +1,179 @@
+﻿namespace app7._5._1
+{
+    public class HugeInteger
+    {
+        byte[] its_nums = new byte[40];
+        byte its_length = 0;
+        bool is_positive = true;
+        public HugeInteger(string hugeint)
+        {
+            this.its_length = (byte)hugeint.Length;
+            for (byte i = 0; i < its_length; i++)
+            {
+                this.its_nums[its_length - i - 1] = Convert.ToByte(hugeint.Substring(i, 1));
+            }
+            for (byte i = its_length; i < 40; i++)
+            {
+                this.its_nums[i] = 0;
+            }
+        }
+        ~HugeInteger()
+        {
+            // Деструктор класса HugeInteger
+        }
+        private void Recount_Length()
+        {
+            byte length = 0;
+            for (byte i = 0; i < 40; i++)
+            {
+                if (its_nums[i] != 0)
+                {
+                    length = i;
+                    length++;
+                }
+            }
+            this.its_length = length;
+        }
+        public void Print()
+        {
+            if (!this.is_positive)
+            {
+                Console.Write("-");
+            }
+            for (byte i = 0; i < this.its_length; i++)
+            {
+                Console.Write(its_nums[this.its_length - i - 1]);
+            }
+        }
+        public string Get()
+        {
+            string returning = "";
+            if (!this.is_positive)
+            {
+                returning += "-";
+            }
+            for (byte i = 0; i < this.its_length; i++)
+            {
+                returning += this.its_nums[this.its_length - i - 1];
+            }
+            return returning;
+        }
+        public static HugeInteger operator +(HugeInteger hi1, HugeInteger hi2)
+        {
+            byte l1 = hi1.its_length;
+            byte l2 = hi2.its_length;
+            HugeInteger sum;
+            if (l1 > l2)
+            {
+                sum = new HugeInteger(hi1.Get());
+                for (byte i = 0; i < l1; i++)
+                {
+                    sum.its_nums[i] += hi2.its_nums[i];
+                    if (sum.its_nums[i] > 9 && (i + 1) < 40)
+                    {
+                        sum.its_nums[i + 1]++;
+                        sum.its_nums[i] -= 10;
+                    }
+                }
+            }
+            else
+            {
+                sum = new HugeInteger(hi2.Get());
+                for (byte i = 0; i < l2; i++)
+                {
+                    sum.its_nums[i] += hi1.its_nums[i];
+                    if (sum.its_nums[i] > 9 && (i + 1) < 40)
+                    {
+                        sum.its_nums[i + 1]++;
+                        sum.its_nums[i] -= 10;
+                    }
+                }
+            }
+            sum.Recount_Length();
+            return sum;
+        }
+        public static HugeInteger operator -(HugeInteger hi1, HugeInteger hi2)
+        {
+            byte l1 = hi1.its_length;
+            byte l2 = hi2.its_length;
+            HugeInteger diff;
+            if (l1 > l2)
+            {
+                diff = new HugeInteger(hi1.Get());
+                for (byte i = 0; i < l1; i++)
+                {
+                    if (diff.its_nums[i] >= hi2.its_nums[i])
+                    {
+                        diff.its_nums[i] -= hi2.its_nums[i];
+                    }
+                    else if (diff.its_nums[i + 1] > 0 && (i + 1) < 40)
+                    {
+                        diff.its_nums[i] = (byte)(diff.its_nums[i] + 10 - hi2.its_nums[i]);
+                        diff.its_nums[i + 1]--;
+                    }
+                    else if(i+2 < l1)
+                    {
+                        byte k = i;
+                        k += 2;
+                        while (k < l1)
+                        {
+                            if (diff.its_nums[k] > 0)
+                            {
+                                diff.its_nums[k]--;
+                                for (byte j = (byte)(k-1); j > i; j--)
+                                {
+                                    diff.its_nums[j] = 9;
+                                }
+                                break;
+                            }
+                            else
+                            {
+                                k++;
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                diff = new HugeInteger(hi2.Get());
+                diff.is_positive = false;
+                for (byte i = 0; i < l2; i++)
+                {
+                    if (diff.its_nums[i] >= hi1.its_nums[i])
+                    {
+                        diff.its_nums[i] -= hi1.its_nums[i];
+                    }
+                    else if (diff.its_nums[i + 1] > 0 && (i + 1) < 40)
+                    {
+                        diff.its_nums[i] = (byte)(diff.its_nums[i] + 10 - hi1.its_nums[i]);
+                        diff.its_nums[i + 1]--;
+                    }
+                    else if (i + 2 < l2)
+                    {
+                        byte k = i;
+                        k += 2;
+                        while (k < l2)
+                        {
+                            if (diff.its_nums[k] > 0)
+                            {
+                                diff.its_nums[k]--;
+                                for (byte j = (byte)(k - 1); j > i; j--)
+                                {
+                                    diff.its_nums[j] = 9;
+                                }
+                                break;
+                            }
+                            else
+                            {
+                                k++;
+                            }
+                        }
+                    }
+                }
+            }
+            diff.Recount_Length();
+            return diff;
+        }
+    }
+}
---
+++ Object Oriented Programming in C #/app7.5.1/app7.5.1/Program.cs
@@ -0,0 +1,188 @@
+﻿using System.Text.RegularExpressions;
+
+namespace app7._5._1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            /* Создайте класс HugeInteger, который использует массив
+                из 40 элементов для хранения целых чисел вплоть до очень больших
+                целых, содержащих до 40 цифр. Создайте функции элементы для
+                ввода, вывода, сложения и вычитания этих чисел. Создайте функции
+                элементы для сравнения этих чисел (для реализации операций =,
+                <>, <, <=, >, >=). Эти функции-предикаты должны возвращать
+                значения Истина (1) или Ложь (0) после выполнения операции
+                сравнения. Создайте предикатную функцию isZero (=0). На основе
+                упомянутых функций создайте функции для умножения и деления
+                таких чисел.
+            */
+            byte answer = 1;
+            List<HugeInteger> list = new List<HugeInteger>();
+            while (answer != 0)
+            {
+                Console.Write("0 - выйти;" +
+                    "\n1 - создать длинное целое число;" +
+                    "\n2 - посмотреть все числа;" +
+                    "\n3 - операции с числами.\n: ");
+                try
+                {
+                    answer = Convert.ToByte(Console.ReadLine());
+                    if (answer > 3)
+                    {
+                        Console.WriteLine("Введено значение больше 3");
+                    }
+                    else
+                    {
+                        switch (answer)
+                        {
+                            case 0:
+                                Environment.Exit(0);
+                                break;
+                            case 1:
+                                Console.Write("Введите длинное целое число: ");
+                                string hugeinteger = "";
+                                hugeinteger += Console.ReadLine();
+                                if (hugeinteger == "")
+                                {
+                                    Console.WriteLine("Значение не введено");
+                                }
+                                else if (Regex.IsMatch(hugeinteger, @"\D", RegexOptions.IgnoreCase))
+                                /* \D : соответствует любому символу, не являющемуся десятичной цифрой */
+                                {
+                                    Console.WriteLine("При вводе присутствовали нечисловые символы");
+                                }
+                                else if (hugeinteger.Length > 40)
+                                {
+                                    Console.Write("Слишком длинное число\n");
+                                }
+                                else
+                                {
+                                    HugeInteger newhi = new HugeInteger(hugeinteger);
+                                    list.Add(newhi);
+                                    Console.WriteLine($"Успешно создано новое большое целое число №{list.Count()}");
+                                    newhi.Print();
+                                    Console.WriteLine();
+                                }
+                                break;
+                            case 2:
+                                for (int i = 0; i < list.Count(); i++)
+                                {
+                                    Console.Write($"{i + 1}:");
+                                    list[i].Print();
+                                    Console.WriteLine();
+                                }
+                                break;
+                            case 3:
+                                Console.Write("1 - арифметические операции" +
+                                    "\n2 - операции сравнения\n:");
+                                byte answer2 = 0;
+                                answer2 = Convert.ToByte(Console.ReadLine());
+                                if (answer2 > 2 || answer2 < 1)
+                                {
+                                    Console.WriteLine("Введено значение не в пределах [1;2]");
+                                }
+                                else
+                                {
+                                    switch (answer2)
+                                    {
+                                        case 1:
+                                            Console.Write("1 - сложение" +
+                                                "\n2 - вычитание" +
+                                                "\n3 - умножение" +
+                                                "\n4 - деление\n:");
+                                            byte answer3 = 0;
+                                            answer3 = Convert.ToByte(Console.ReadLine());
+                                            if (answer3 > 4 || answer3 < 1)
+                                            {
+                                                Console.WriteLine("Введено значение не в пределах [1;4]");
+                                            }
+                                            else
+                                            {
+                                                switch (answer3)
+                                                {
+                                                    case 1:
+                                                        Console.Write("Выберите первое слагаемое: ");
+                                                        byte sum1 = 0;
+                                                        sum1 = Convert.ToByte(Console.ReadLine());
+                                                        if (sum1 > list.Count())
+                                                        {
+                                                            Console.WriteLine("Такого числа нет в списке");
+                                                        }
+                                                        else
+                                                        {
+                                                            Console.Write($"Первое слагаемое - число №{sum1} = {list[sum1 - 1].Get()}");
+                                                            Console.Write("\nВыберите второе слагаемое: ");
+                                                            byte sum2 = 0;
+                                                            sum2 = Convert.ToByte(Console.ReadLine());
+                                                            if (sum2 > list.Count())
+                                                            {
+                                                                Console.WriteLine("Такого числа нет в списке");
+                                                            }
+                                                            else
+                                                            {
+                                                                Console.Write($"Второе слагаемое - число №{sum2} = {list[sum2 - 1].Get()}");
+                                                                HugeInteger sum = list[sum1 - 1] + list[sum2 - 1];
+                                                                Console.WriteLine($"\nСумма слагаемых №{sum1} и №{sum2} = {sum.Get()}");
+                                                            }
+                                                        }
+                                                        break;
+                                                    case 2:
+                                                        Console.Write("Выберите уменьшаемое: ");
+                                                        byte dif1 = 0;
+                                                        dif1 = Convert.ToByte(Console.ReadLine());
+                                                        if (dif1 > list.Count())
+                                                        {
+                                                            Console.WriteLine("Такого числа нет в списке");
+                                                        }
+                                                        else
+                                                        {
+                                                            Console.Write($"Уменьшаемое - число №{dif1} = {list[dif1 - 1].Get()}");
+                                                            Console.Write("\nВыберите вычитаемое: ");
+                                                            byte dif2 = 0;
+                                                            dif2 = Convert.ToByte(Console.ReadLine());
+                                                            if (dif2 > list.Count())
+                                                            {
+                                                                Console.WriteLine("Такого числа нет в списке");
+                                                            }
+                                                            else
+                                                            {
+                                                                Console.Write($"Вычитаемое - число №{dif2} = {list[dif2 - 1].Get()}");
+                                                                HugeInteger dif = list[dif1 - 1] - list[dif2 - 1];
+                                                                Console.WriteLine($"\nРазность чисел №{dif1} и №{dif2} = {dif.Get()}");
+                                                            }
+                                                        }
+                                                        break;
+                                                    case 3:
+                                                        break;
+                                                    case 4:
+                                                        break;
+                                                }
+                                            }
+                                            break;
+                                        case 2:
+                                            Console.Write("1 - найти большее число из двух" +
+                                                "\n2 - найти равенство двух чисел" +
+                                                "\n3 - найти неравенство двух чисел" +
+                                                "\n4 - найти равенство нулю" +
+                                                "\n5 - Число 1 >= числа 2" +
+                                                "\n6 - Число 1 <= числа 2\n:");
+                                            break;
+                                    }
+                                }
+                                break;
+                        }
+                    }
+                }
+                catch (FormatException Ex)
+                {
+                    Console.WriteLine(Ex.Message);
+                }
+                catch (OverflowException Ex)
+                {
+                    Console.WriteLine(Ex.Message);
+                }
+            }
+        }
+    }
+}

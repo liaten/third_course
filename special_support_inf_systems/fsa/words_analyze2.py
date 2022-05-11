9c3fc31f5acfe48e4e2a99e8ddf951aefb346478
Vladislav <vlad171vlad@gmail.com>
2022-05-11 18:28:35 +0300

    выделил отдельную сущность
    тк используется больше 1 раза

--- special_support_inf_systems/fsa/words_analyze2.py
+++ special_support_inf_systems/fsa/words_analyze2.py
@@ -3,4 +3,5 @@ from wikipedia import search
 from spacy import load
 for word in load("ru_core_news_lg")(input()):
   if word.pos_ == 'NOUN':
-    print(MorphAnalyzer().parse(word.text)[0].normal_form,search(MorphAnalyzer().parse(word.text)[0].normal_form))
+    noun = MorphAnalyzer().parse(word.text)[0].normal_form
+    print(noun,search(noun))

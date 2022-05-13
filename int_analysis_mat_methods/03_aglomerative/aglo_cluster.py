4273b454372ecb9ff47c1442cb4d4ce1f7e6fd1f
liaten <vlad171vlad@gmail.com>
2022-05-13 09:50:23 +0300

    алгосы алгосики

--- int_analysis_mat_methods/03_aglomerative/aglo_cluster.py
+++ int_analysis_mat_methods/03_aglomerative/aglo_cluster.py
@@ -1,13 +1,22 @@
+# ГЛАВНЫЙ ФАЙЛ
 from openpyxl import load_workbook
 from distances import distances
 from distances import print_array
 
+def euclid_distance(c1, c2):
+    distance = 0
+    for i in range(4):
+        distance += (c1[i] - c2[i])**2
+    return distance**(1/2)
+
+# Читаем эксель-файл
 wb = load_workbook(filename = 'irisy.xlsx')
 l = wb['l']
 
 iris_coordinates = []
 iris_classes = []
 
+# Заполняем списки с координатами и классами ирисов
 for i in range(2,122):
     length1 = l.cell(row=i,column=1).value
     width1 = l.cell(row=i,column=2).value
@@ -17,12 +26,7 @@ for i in range(2,122):
     coords = [length1, width1, length2, width2, iris_class]
     iris_coordinates.append(coords)
 
-def euclid_distance(c1, c2):
-    distance = 0
-    for i in range(4):
-        distance += (c1[i] - c2[i])**2
-    return distance**(1/2)
-
+# Считаем расстояние между ирисами методом Евклида
 iris_size = len(iris_coordinates)
 dis = [0]*(iris_size)
 i = 0
@@ -37,6 +41,6 @@ for c1 in iris_coordinates:
     iris_classes.append(c1[4])
     dis[i] = dist
     i += 1
+# Методом агломерационной кластеризации находим основные кластеры и их расстояние между друг другом
 dis = distances(dis,iris_classes,3)
-print_array(dis)
-# print(iris_coordinates[0])
+print_array(dis, iris_classes)
--- int_analysis_mat_methods/03_aglomerative/distances.py
+++ int_analysis_mat_methods/03_aglomerative/distances.py
@@ -1,5 +1,4 @@
-from warnings import catch_warnings
-
+# ЭТОТ ФАЙЛ НАПИСАН В КАЧЕСТВЕ БИБЛИОТЕКИ К ФАЙЛУ ALGO_CLUSTER
 def min_item_in_list_by_id(list1, list2, id):
     if(list1[id]<list2[id]):
         return list1[id]
@@ -10,14 +9,10 @@ def min_distance(dist):
     min_c = []
     for e in dist:
         for el in e:
-            try:
-                if(float(el)<min_dist):
-                    min_dist = el
-                    min_c = [dist.index(e),e.index(el)]
-            except ValueError:
-                pass
+            if(el<min_dist):
+                min_dist = el
+                min_c = [dist.index(e),e.index(el)]
     min_c.sort()
-    # print(min_c)
     return min_c
 
 def move_to_top(dist,min_c):
@@ -30,6 +25,11 @@ def move_to_start(dist,min_c):
 def print_array(array):
     for element in array:
         print(element)
+    print()
+
+def print_array(array, classes):
+    for i in range(len(array)):
+        print(array[i], classes[i])
     print()
 
 def create_smallest_list(lists,new_len):
@@ -52,30 +52,22 @@ def distances(dist_input, classes_input, N):
     while(len(dist)>N):
         min_c = min_distance(dist) # Находим минимальное число
         if(classes[min_c[0]]!=classes[min_c[1]]):
-            errors_classes +=1
+            errors_classes +=1 # Считаем ошибки - случаи, когда происходит слияние разных классов
+        classes[0],classes[min_c[0]] = classes[min_c[0]],classes[0]
         classes.pop(min_c[1]) # Удаляю из списка классов оставшийся
-        operations_classes +=1
+        operations_classes +=1 # Считаем операции с классами
         move_to_top(dist,min_c) # Двигаем наверх строчки с минимумами
         move_to_start(dist,min_c) # Меняем местами 2 первых столбца и столбцы с минимумами
         new_len = len(dist)-2 # Считаем размер финального массива
         save_dist = [e[-new_len:] for e in dist[-new_len:]] # Сохраняем строчки, которые будут в новом массиве
-        # print_array(save_dist)
         lists = [(dist[e])[-new_len:] for e in [0,1]]   # Берем с конца первых двух строчек необходимое количество элементов для поиска наименьших значений
         smallest_list = create_smallest_list(lists,new_len) # Создаём первую строчку/столбец нового массива
-        # print_array(smallest_list)
         dist = upgrade_dist(dist,smallest_list,new_len,save_dist) # Обновляем старый массив новыми данными
-        # print_array(dist) # Выводим результат на экран
     error_percent = errors_classes/operations_classes*100
     print('Процент слияний разных классов =',error_percent,'%')
     return dist
 
-# dist = [
-#         [1000,17,21,31,23,'Iris-setosa'],
-#         [17,1000,30,34,21,'Iris-versicolor'],
-#         [21,30,1000,28,39,'Iris-versicolor'],
-#         [31,34,28,1000,43,'Iris-virginica'],
-#         [23,21,39,43,1000,'Iris-virginica'],
-#     ]
+# Для тестирования работы алгоритма:
 # dist = [
 #         [1000,17,21,31,23],
 #         [17,1000,30,34,21],
